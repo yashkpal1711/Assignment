d0abf11a098153e7371367fc5c89ebf3aee71850
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-13 22:04:36 +0530

    Update HTML, CSS, and JavaScript code for product page

--- Script.js
+++ Script.js
@@ -1,12 +1,31 @@
-const headerButtons = document.querySelectorAll('.header-button');
-const overlay = document.querySelector('.overlay');
+const headerButtons = document.querySelectorAll(".header-button");
+const overlay = document.querySelector(".overlay");
 
-headerButtons.forEach(button => {
-  button.addEventListener('mouseover', () => {
-    overlay.style.display = 'block';
+headerButtons.forEach((button) => {
+  button.addEventListener("mouseover", () => {
+    overlay.style.display = "block";
   });
 
-  button.addEventListener('mouseout', () => {
-    overlay.style.display = 'none';
+  button.addEventListener("mouseout", () => {
+    overlay.style.display = "none";
+  });
+});
+
+const accordion = document.getElementsByClassName("contentBx");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
+const image = document.getElementById("image");
+const radioButton = document.querySelectorAll("input[type=radio]");
+
+radioButton.forEach(function (radioButtonaq) {
+  radioButtonaq.addEventListener("change", function () {
+    if (this.checked) {
+      image.src = this.value;
+    }
   });
 });
