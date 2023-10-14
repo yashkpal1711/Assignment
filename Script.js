1dfb23b49d97bbc589ecbb94133e17a73c7b8f22
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-14 12:31:17 +0530

    pre final

--- Script.js
+++ Script.js
@@ -1,14 +1,14 @@
-const pinkButton = document.getElementById('pinkButton');
-const body = document.getElementById('body-container');
+const pinkButton = document.getElementById("pinkButton");
+const bodyContainer = document.getElementById("body-container");
 
 // Function to toggle the pink-background class
 function togglePinkBackground() {
-    body.classList.toggle('pink-background');
+  bodyContainer.classList.toggle("pink-background");
 }
 
 // Event listeners
-pinkButton.addEventListener('mouseenter', togglePinkBackground);
-pinkButton.addEventListener('mouseleave', togglePinkBackground);
+pinkButton.addEventListener("mouseenter", togglePinkBackground);
+pinkButton.addEventListener("mouseleave", togglePinkBackground);
 
 const accordion = document.getElementsByClassName("contentBx");
 
@@ -28,3 +28,98 @@ radioButton.forEach(function (radioButtonaq) {
     }
   });
 });
+
+// CART
+let openShopping = document.querySelector(".shopping");
+let closeShopping = document.querySelector(".closeShopping");
+let list = document.querySelector(".list");
+let listCard = document.querySelector(".listCard");
+let body = document.querySelector("body");
+let total = document.querySelector(".total");
+let quantity = document.querySelector(".quantity");
+let closeCart = document.getElementById("closeCart");
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active");
+  bodyContainer.classList.toggle("pink-background");
+});
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active");
+  bodyContainer.classList.toggle("pink-background");
+});
+closeCart.addEventListener("click", () => {
+  body.classList.remove("active");
+  bodyContainer.classList.toggle("pink-background");
+});
+
+let products = [
+  {
+    id: 1,
+    name: "Spiced Blend",
+    image:
+      "https://decemdrinks.com/cdn/shop/products/SpicedBlend_Serve_1024x1024.png?v=1659602678",
+    price: 29.95,
+  },
+];
+
+let listCards = [];
+function initApp() {
+  products.forEach((value, key) => {
+    const cartBtn = document.getElementById("addCart");
+
+    addCart.classList.add("item");
+
+    cartBtn.addEventListener("click", () => {
+      bodyContainer.classList.toggle("pink-background");
+      body.classList.add("active");
+      addToCard(key);
+    });
+  });
+}
+initApp();
+
+function addToCard(key) {
+  if (listCards[key] == null) {
+    // copy product form list to list card
+    listCards[key] = JSON.parse(JSON.stringify(products[key]));
+    listCards[key].quantity = 1;
+  }
+  reloadCard();
+}
+function reloadCard() {
+  listCard.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key) => {
+    totalPrice = totalPrice + value.price;
+    count = count + value.quantity;
+    if (value != null) {
+      let newDiv = document.createElement("li");
+      newDiv.innerHTML = `
+                <div><img src="${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>£ ${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity - 1
+      })">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity + 1
+      })">+</button>
+                </div>`;
+      listCard.appendChild(newDiv);
+    }
+  });
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete listCards[key];
+  } else {
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
